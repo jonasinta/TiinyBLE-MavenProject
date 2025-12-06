3632301f586fef61795745e853838d0e4eba92b7
jonas <wompusi@gmail.com>
2025-12-06 23:34:16 +1300

    first commit hello

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,96 @@
+package org.example;
+
+import tinyb.BluetoothDevice;
+import tinyb.BluetoothException;
+import tinyb.BluetoothManager;
+
+import java.time.Duration;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static boolean running = true;
+
+    static BluetoothDevice getDevice(String address) throws InterruptedException  {
+        BluetoothManager manager = BluetoothManager.getBluetoothManager();
+        BluetoothDevice sensor = null;
+        for (int i = 0; (i < 15) && running; ++i) {
+            List<BluetoothDevice> list = manager.getDevices();
+            for (BluetoothDevice device : list) {
+                printDevice(device);
+                /*
+                 * Here we check if the address matches.
+                 */
+                if (device.getAddress().equals(address))
+                    sensor = device;
+            }
+            if (sensor != null) {
+                return sensor;
+            }
+            Thread.sleep(4000);
+        }
+        return null;
+    }
+
+    static void printDevice(BluetoothDevice device) {
+        System.out.print("Address = " + device.getAddress());
+        System.out.print(" Name = " + device.getName());
+        System.out.print(" Connected = " + device.getConnected());
+        System.out.println();
+    }
+    public static void main(String[] args) throws InterruptedException {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!", args[0]);
+        System.out.println("");
+        if (args.length < 1) {
+            System.err.println("Run with <device_address> argument");
+            System.exit(-1);
+        }
+        System.out.println("argument enetred;");
+        System.out.println(args[0]);
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+        // start tinyb little bits here
+        BluetoothManager manager = BluetoothManager.getBluetoothManager();
+        boolean discoveryStarted = manager.startDiscovery();
+        System.out.println("boolean output next line");
+        System.out.println(discoveryStarted);
+
+
+        /*
+         * After discovery is started, new devices will be detected. We can find the device we are interested in
+         * through the manager's find method.
+         */
+        BluetoothDevice sensor = manager.find(null, args[0], null, Duration.ofSeconds(10));
+
+        /*
+         * After we find the device we can stop looking for other devices.
+         */
+        try {
+            manager.stopDiscovery();
+        } catch ( BluetoothException e) {
+            System.err.println("Discovery could not be stopped right now");
+        }
+
+        if (sensor == null) {
+            System.err.println("No sensor found with the provided address.");
+            System.exit(-1);
+        }
+
+        System.out.print("Found device: ");
+        printDevice(sensor);
+
+        if (sensor.connect())
+            System.out.println("Sensor with the provided address connected");
+        else {
+            System.out.println("Could not connect device.");
+            System.exit(-1);
+        }
+    }
+
+}
