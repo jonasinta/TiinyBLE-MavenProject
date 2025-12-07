282d4573306ed401a6c99ee6eb0f258954d713f7
jonas <wompusi@gmail.com>
2025-12-07 15:28:16 +1300

    addedtest dependencies

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getDevice() {
+    }
+
+    @Test
+    void printDevice() {
+    }
+
+    @Test
+    void main() {
+    }
+}
